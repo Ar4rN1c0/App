672b76155a102d67e0ff7447d92ff711c66b3016
4r4rN1c0 <nicolas.arnaiz.arce@gmail.com>
2024-06-04 10:07:17 +0200

    TS SOLVED

--- Actions/registerChat.ts
+++ Actions/registerChat.ts
@@ -5,7 +5,10 @@ import getUserInfo from "./getUserInfo";
 
 export default async function registerChat() {
     const session = await getServerSession()
-    const user = await getUserInfo()
+    const user = await getUserInfo();
+    if (!user || typeof user !== 'object') {
+        throw new Error("User information could not be retrieved");
+    }
     const existingNewChat = await prisma.chat.findFirst({where: {authorId: user?.id, title: "New Chat"}})
     if(!existingNewChat && session) await prisma.chat.create({data: {authorId: user?.id as string, title: "New Chat"}})
 }
